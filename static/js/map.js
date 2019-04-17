2b8c84a641cb42ae2bd18073f6d5934a106295bf
Oscar Padilla <padillaoscara@gmail.com>
2019-04-17 02:04:18 -0400

    d3 us map added

--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,32 @@
 // document.write("map.js content")
 
-var data = [30, 86, 168, 281, 303, 435]
+// var data = [30, 86, 168, 281, 303, 435]
+//
+// d3.select(".map-chart")
+//     .selectAll("div")
+//     .data(data)
+//         .enter()
+//         .append("div")
+//         .style("width", function(d) { return d + "px"; })
+//         .text(function(d) { return d; });
 
-d3.select(".map-chart")
-    .selectAll("div")
-    .data(data)
-        .enter()
-        .append("div")
-        .style("width", function(d) { return d + "px"; })
-        .text(function(d) { return d; });
+var svg = d3.select("svg");
+
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+    if (error) throw error;
+
+    svg.append("g")
+        .attr("class", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+        .attr("d", path)
+
+    svg.append("path")
+        .attr("class", "state-borders")
+        .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) {
+            return a !== b;
+        })));
+});
