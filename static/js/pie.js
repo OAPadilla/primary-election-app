cd60c2071642c5f004c06da1b007014f350ba5be
Oscar Padilla <padillaoscara@gmail.com>
2019-04-30 03:16:55 -0400

    pie chart outputs info

--- static/js/pie.js
+++ static/js/pie.js
@@ -1,18 +1,21 @@
+
+// var data = [{"name": "Joe Biden", "color": "#009933", "poll": 51},
+//             {"name": "Bernie Sanders", "color": "#6699ff", "poll": 24},
+//             {"name": "Kamala Harris", "color": "#ff9966", "poll": 11}];
+
+//FIXME: Append total delegate counts to data holding candidateData
+
 // Candidate data for pie chart
-var data = [{"name": "Joe Biden", "color": "#009933", "poll": 51},
-            {"name": "Bernie Sanders", "color": "#6699ff", "poll": 24},
-            {"name": "Kamala Harris", "color": "#ff9966", "poll": 11}];
-console.log(data)
+var data = candidateData
 
 // Set size and attributes of pie chart
 var width = 640;
 var height = 360;
 var radius = Math.min(width, height) / 2;
-// var opacity = 0.8;
-// var opacityHover = 1;
-// var otherOpacity = 0.8;
+var opacity = 1;
+var opacityHover = 0.7;
 
-// Create shapes
+// Create pie shape holding data
 var pie = d3.pie()
     .value(function(d) {
         return d.poll;
@@ -21,12 +24,12 @@ var pie = d3.pie()
 // Arc of pie circle
 var arc = d3.arc()
     .outerRadius(radius - 10)
-    .innerRadius(radius * 0.4);
+    .innerRadius(radius * 0.45);
 
-// Arc of label in pie
+// Arc of labels in pie
 var labelArc = d3.arc()
-    .outerRadius(radius - 40)
-    .innerRadius(radius - 40);
+    .outerRadius(radius - 50)
+    .innerRadius(radius - 50);
 
 // Set Pie SVG
 var pieSVG = d3.select("#pie-chart").append("svg")
@@ -40,14 +43,36 @@ var g = pieSVG.selectAll("arc")
     .data(pie)
     .enter()
     .append("g")
-    .attr("class", "arc");
+    .attr("class", "arc")
 
-// Add color to paths created with variables in data
+// Create center candidate data in donut hole
+d3.select("#pie-chart").select("svg")
+    .append("g")
+    .append("text")
+    .attr("class", "candidate-text")
+    .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+    .text(``)
+    .attr('text-anchor', 'middle')
+
+// Add color/style with variables of data
 g.append("path")
     .attr("d", arc)
     .style("fill", function(d) {
         return d.data.color;
-    });
+    })
+    .style("opacity", opacity)
+    .on("mouseover", function(d) {
+        d3.select(this).style("opacity", opacityHover);
+        // Display candidate, percentage, delegates won in donut hole
+        d3.select(".candidate-text")
+            .text(`${d.data.name} (${d.data.poll})`)
+    })
+    .on("mouseout", function(d) {
+        d3.select(this).style("opacity", opacity)
+        // Clear displayed candidate in donut hole
+        d3.select(".candidate-text")
+            .text('')
+    })
 
 // Labels
 g.append("text")
@@ -55,6 +80,7 @@ g.append("text")
         return "translate(" + labelArc.centroid(d) + 40 + ")";
     })
     .text(function(d) {
-        return d.data.poll + "%";
+        if (d.data.poll >= 5) {
+            return d.data.poll + "%";
+        }
     })
-    .style("fill", "white")
